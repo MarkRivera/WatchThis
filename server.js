788c6e71f0c3aa6f8539cdbcbc049b910531bf9b
Mark Rivera <Mrivera1991@gmail.com>
2019-08-07 14:46:59 -0400

    watch this app completed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const watchThis = express();
 
@@ -8,19 +9,24 @@ connectDB();
 
 // Init middleware
 
-watchThis.use(express.json({extended: false}));
-
-watchThis.get("/", (req, res) => {
-	res.send("API running");
-});
-
+watchThis.use(express.json({ extended: false }));
 
 // Definite routes to connect to different aspects of API:
-watchThis.use('/api/users', require('./routes/api/users'));
-watchThis.use('/api/auth', require('./routes/api/auth'));
-watchThis.use('/api/profile', require('./routes/api/profiles'));
-watchThis.use('/api/movies', require('./routes/api/movies'));
-watchThis.use('/api/genres', require('./routes/api/genres'));
+watchThis.use("/api/users", require("./routes/api/users"));
+watchThis.use("/api/auth", require("./routes/api/auth"));
+watchThis.use("/api/profile", require("./routes/api/profiles"));
+watchThis.use("/api/movies", require("./routes/api/movies"));
+watchThis.use("/api/genres", require("./routes/api/genres"));
+
+//Serve static assets in production:
+
+if (process.env.NODE_ENV === "production") {
+	watchThis.use(express.static("client/build"));
+
+	watchThis.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
